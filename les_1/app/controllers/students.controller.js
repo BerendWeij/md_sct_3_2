469f98d40164c4c62474be68e5265cb35026dc63
Berend Weij <info@berendweij.nl>
2016-05-09 08:34:23 +0200

    added lesson 1

---
+++ les_1/app/controllers/students.controller.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('controllers')
+        .controller('StudentsController', StudentsController);
+
+    function StudentsController() {
+
+        var vm = this;
+
+        vm.studentName = "Gianni";
+
+    }
+
+})();
