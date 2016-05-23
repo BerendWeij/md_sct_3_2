76c8eb02a71373ba31ca102af8b94edf448e0b9e
Berend Weij <info@berendweij.nl>
2016-05-23 12:40:16 +0200

    switched back to start of the lesson

--- studentsApp/app/controllers/students.controller.js
+++ studentsApp/app/controllers/students.controller.js
@@ -9,34 +9,14 @@
 
         var vm = this;
 
-        vm.newStudent = {};
-
-        vm.setCurrentStudent = function(currentStudent){
-            if(currentStudent != vm.currentStudent)
-                vm.currentStudent = currentStudent;
-            else
-                vm.currentStudent = undefined;
-        };
-
-        vm.addStudent = function(){
-            vm.allStudents.push(
-                {
-                    name: vm.newStudent.name,
-                    age: vm.newStudent.age
-                }
-            );
-
-            vm.newStudent = {};
-        };
-
         vm.allStudents = [
-            {name:"Artur", age:20},
-            {name:"Mitchell", age:18},
-            {name:"Remco", age:17},
-            {name:"Dave", age:21},
-            {name:"Sander", age:17},
-            {name:"Jimmy", age:18},
-            {name:"Yassine", age:19}
+            "Anton",
+            "Joris",
+            "Cees",
+            "Ryan",
+            "Jesse",
+            "Idris",
+            "Donovan"
         ];
 
 
