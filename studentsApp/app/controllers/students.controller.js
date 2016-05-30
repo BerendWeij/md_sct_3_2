dc773d946c6c5e392169bbb176641e36c34b893c
Berend Weij <info@berendweij.nl>
2016-05-30 10:25:24 +0200

    added example of

--- studentsApp/app/controllers/students.controller.js
+++ studentsApp/app/controllers/students.controller.js
@@ -5,7 +5,7 @@
         .module('controllers')
         .controller('StudentsController', StudentsController);
 
-    function StudentsController() {
+    function StudentsController($http) {
 
         var vm = this;
 
@@ -20,11 +20,13 @@
         };
 
         vm.createStudent = function(){
-            vm.allStudents.push(
-                {
-                    name: vm.newStudent.name,
-                    age: vm.newStudent.age
-                });
+            var newStudent = {
+                name: vm.newStudent.name,
+                age: vm.newStudent.age
+            };
+
+            //vm.allStudents.push(newStudent);
+            vm.allStudents.splice(0, 0, newStudent);
 
             vm.newStudent = {};
         }
@@ -39,6 +41,13 @@
             {name:"Donovan", age:18}
         ];
 
+
+        // $http.get() -> returned een object {then:function(){}}
+        // dit object noemen we een promise
+        // met then() geven we aan welke functie we willen uitvoeren als $http klaar is met laden
+        $http.get('data/trainstations.json').then(function(trainstations){
+            vm.allStudents = trainstations.data;
+        });
 
     }
 
