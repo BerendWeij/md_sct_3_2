ca094f5d154e4e69829f4bc44c2987521eadaf04
Berend Weij <info@berendweij.nl>
2016-05-30 13:50:44 +0200

    added angular animate example

--- studentsApp/app/controllers/students.controller.js
+++ studentsApp/app/controllers/students.controller.js
@@ -5,7 +5,7 @@
         .module('controllers')
         .controller('StudentsController', StudentsController);
 
-    function StudentsController($http) {
+    function StudentsController() {
 
         var vm = this;
 
@@ -23,10 +23,10 @@
             var newStudent = {
                 name: vm.newStudent.name,
                 age: vm.newStudent.age
-            };
+            }
 
-            //vm.allStudents.push(newStudent);
             vm.allStudents.splice(0, 0, newStudent);
+            //vm.allStudents.push(newStudent);
 
             vm.newStudent = {};
         }
@@ -41,13 +41,6 @@
             {name:"Donovan", age:18}
         ];
 
-
-        // $http.get() -> returned een object {then:function(){}}
-        // dit object noemen we een promise
-        // met then() geven we aan welke functie we willen uitvoeren als $http klaar is met laden
-        $http.get('data/trainstations.json').then(function(trainstations){
-            vm.allStudents = trainstations.data;
-        });
 
     }
 
